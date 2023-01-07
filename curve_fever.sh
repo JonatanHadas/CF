e04c13093278410788f145356358e3cbab355ef0
Jonatan Hadas <github@jonatan.hadaso.net>
2023-01-07 15:13:45 +0200

    Use host's /dev on docker to allow hardware acceleration

--- curve_fever.sh
+++ curve_fever.sh
@@ -1,6 +1,7 @@
 docker run \
 	-v "$PWD"/game_data:/usr/src/app/game_data \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v /dev:/dev \
 	--env DISPLAY="$DISPLAY" \
 	--net=host \
 	-it --rm jonatan_h/curve-fever
